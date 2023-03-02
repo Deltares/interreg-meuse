b09ab15f8eb2c26bafda19737d7a7590ff5e29ff
AlessiaRiveros <alessia@riveros.ca>
2023-03-02 13:44:26 +0100

    updated to create a nc similar to spw_statistiques_to_nc

---
+++ src/1-prepare/hydro_statistiques_to_nc.py
@@ -0,0 +1,63 @@
+import xarray as xr
+import pandas as pd
+import os
+import glob
+
+folder_p = r'p:\11208719-interreg\data\hydroportail\a_raw'
+folder_save = r'p:\11208719-interreg\data\hydroportail\b_final'
+
+daily = 'QJ-X_(CRUCAL)'
+hourly = 'Q-X_(CRUCAL)'
+
+timestep = hourly  # or hourly
+
+if timestep == daily:
+    timestep_name = "daily"
+else:
+    timestep_name = "hourly"
+
+# add years and hydrological years
+# add x y coordinates
+
+file1 = os.path.join(folder_p, 'la_meuse_a_chooz', daily, 'QJ-X-(CRUCAL)_Gumbel_B7200000_01-01-1953_06-02-2023_1_non-glissant_X_pre-valide-et-valide_ResultatsAjustement.csv')
+file2 = os.path.join(folder_p, 'la_meuse_a_goncourt', daily, 'QJ-X-(CRUCAL)_Gumbel_B022001001_15-09-1971_06-02-2023_1_non-glissant_X_pre-valide-et-valide_ResultatsAjustement.csv')
+
+df1 = pd.read_csv(file1, index_col='Période de retour')
+df2 = pd.read_csv(file2, index_col='Période de retour')
+
+df1.index.rename('T', inplace=True)
+df2.index.rename('T', inplace=True)
+
+df1.rename(columns={'Valeur ajustée (en m³/s)': 'valeur', 'Intervalle de confiance bas (en m³/s)': 'int_conf_bas',
+                    'Intervalle de confiance haut (en m³/s)': 'int_conf_haut'}, inplace=True)
+df2.rename(columns={'Valeur ajustée (en m³/s)': 'valeur', 'Intervalle de confiance bas (en m³/s)': 'int_conf_bas',
+                    'Intervalle de confiance haut (en m³/s)': 'int_conf_haut'}, inplace=True)
+
+df1.drop('Fréquence au non dépassement', axis=1, inplace=True)
+df2.drop('Fréquence au non dépassement', axis=1, inplace=True)
+
+da1 = df1.to_xarray()
+da2 = df2.to_xarray()
+
+da1 = da1.assign(name='la_meuse_a_chooz')
+da2 = da1.assign(name='la_meuse_a_goncourt')
+
+lat1 = 7000411
+lon1 = 827688
+
+lat2 = 6796620
+lon2 = 894048
+
+da1 = da1.assign_coords(coords={"lat": lat1})
+da1 = da1.assign_coords(coords={"lon": lon1})
+
+da2 = da2.assign_coords(coords={"lat": lat2})
+da2 = da2.assign_coords(coords={"lon": lon2})
+
+
+ds = xr.concat([da1, da2], dim='station')
+ds['valeur'].attrs = {"units": "m³/s", "description": "valeur ajustée avec la loi de Gumbel estimée par la méthode L-moments"}
+ds['int_conf_haut'].attrs = {"units": "m³/s", "description": "intervalle de confiance haut à 95% quantifiée par la méthode Bootstrap paramétrique"}
+ds['int_conf_bas'].attrs = {"units": "m³/s", "description": "intervalle de confiance bas à 95% quantifiée par la méthode Bootstrap paramétrique"}
+
+# ds.to_netcdf(os.path.join(folder_save, timestep_name, 'statistiques_' + timestep_name + '.nc'))
