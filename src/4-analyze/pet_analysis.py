9a2db507ecd9ec4f07e625e8e1004a6020f85e48
AlessiaRiveros <alessia@riveros.ca>
2023-03-17 17:19:30 +0100

    create 3 plots for pet forcing

--- src/4-analyze/pet_analysis.py
+++ src/4-analyze/pet_analysis.py
@@ -12,55 +12,80 @@ cdir = r'c:\Projects\interreg\local\interreg-meuse\wflow'
 case_debruin = 'run_debruin'
 case_makkink = 'run_makkink'
 
-staticmaps_debruin = r'inmaps_eobs_v25.0e_era5_daily_zarrd_debruin_86400_1980_2021.nc'
-staticmaps_makkink = r'inmaps_eobs_v25.0e_era5_daily_zarrd_makkink_86400_1980_2021.nc'
-# staticmaps_penman = ''
+staticmaps = r'eobs_v25.0e_1980_2020.nc'
 
-ds_debruin = xr.open_dataset(os.path.join(cdir, case_debruin, staticmaps_debruin))
-ds_makkink = xr.open_dataset(os.path.join(cdir, case_makkink, staticmaps_makkink))
-# ds_penman = xr.open_dataset(os.path.join(cdir, case_makkink, staticmaps_penman))
-
-pet_test = ds_debruin['pet'].sel(time=slice("1981-01-01", "1982-01-01")).mean('time')
-fig, ax = plt.subplots()
-pet_test.plot.pcolormesh(ax=ax, cmap='Spectral_r', add_colorbar=True, extend='both')
-
-pet_test = ds_debruin['pet'].sel(lat=50.1, lon=5, method='nearest')
-fig, ax = plt.subplots()
-pet_test.plot()
+ds_debruin = xr.open_dataset(os.path.join(cdir, case_debruin, staticmaps))
+ds_makkink = xr.open_dataset(os.path.join(cdir, case_makkink, staticmaps))
+# ds_penman = xr.open_dataset(os.path.join(cdir, case_penman, staticmaps))
+ds = ds_makkink
 
 # mean monthly values
-# df_m = df.resample('m').sum()
-# df_month = df_m.groupby(df_m.index.month).mean()
-pet_monthly = ds_debruin['pet'].groupby('time.month').mean('time')
+pet_monthly = ds['pet'].resample(time='m').sum('time').groupby('time.month').mean('time')
+# when summing, np.nan -> 0, to exclude
+pet_monthly = pet_monthly.where(pet_monthly != 0)
+vmin = 10
+vmax = 100
+
 fig, axes = plt.subplots(nrows=2, ncols=6, figsize=(11, 6))
 month_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
               'November', 'December']
 for i in range(12):
     month = i + 1  # starts at month 1
-    pet_monthly.sel(month=month).plot.pcolormesh(
-        ax=axes[i//6, i%6], cmap='Spectral_r', add_colorbar=True, extend='both')
-    axes[i//6, i%6].set_title(month_name[i])
-    axes[i//6, i%6].set_xlabel('')
-    axes[i//6, i%6].set_ylabel('')
+    # to remove tick labels, keep y only for column 0 and keep x only for row 0
+    if i % 6 > 0:
+        axes[i // 6, i % 6].yaxis.set_ticklabels([])
 
-plt.tight_layout()
+    if i//6 < 1:
+        axes[i // 6, i % 6].xaxis.set_ticklabels([])
+
+    if i % 6 > 4:
+        pet_monthly.sel(month=month).plot.pcolormesh(
+            ax=axes[i // 6, i % 6], cmap='Spectral_r', add_colorbar=True, vmin=vmin, vmax=vmax, extend='both')
+    else:
+        pet_monthly.sel(month=month).plot.pcolormesh(
+            ax=axes[i // 6, i % 6], cmap='Spectral_r', add_colorbar=False, vmin=vmin, vmax=vmax, extend='both')
+
+    axes[i // 6, i % 6].set_title(month_name[i])
+    axes[i // 6, i % 6].set_xlabel('')
+    axes[i // 6, i % 6].set_ylabel('')
 
 # mean annual values
 start_year = 1980
 end_year = 2021
-pet_yearly = ds_debruin['pet'].groupby('time.year').mean('time')
+pet_yearly = ds['pet'].resample(time='y').sum('time').groupby('time.year').mean('time')
+pet_yearly = pet_yearly.where(pet_yearly != 0)
+vmin = 520
+vmax = 720
 
-fig, axes = plt.subplots(nrows=4, ncols=10, figsize=(11, 6))
-for i in range(40):
-    year = np.arange(start_year, end_year, 1)
-    pet_yearly.sel(year=year[i]).plot.pcolormesh(
-        ax=axes[i//10, i % 10], cmap='Spectral_r', add_colorbar=True, extend='both')
-    axes[i//10, i % 10].set_title(str(year[i]))
-    axes[i//10, i % 10].set_xlabel('')
-    axes[i//10, i % 10].set_ylabel('')
+fig, axes = plt.subplots(nrows=4, ncols=11, figsize=(12, 8.5))
+for i in range(end_year + 1 - start_year):
+    year = np.arange(start_year, end_year + 1, 1)
+
+    if i % 11 > 0:
+        axes[i//11, i % 11].yaxis.set_ticklabels([])
+    if i // 11 < 3:
+        axes[i//11, i % 11].xaxis.set_ticklabels([])
+
+    if i % 11 > 9:
+        pet_yearly.sel(year=year[i]).plot.pcolormesh(
+                ax=axes[i//11, i % 11], cmap='Spectral_r', add_colorbar=True, vmin=vmin, vmax=vmax, extend='both')
+    else:
+        pet_yearly.sel(year=year[i]).plot.pcolormesh(
+                ax=axes[i//11, i % 11], cmap='Spectral_r', add_colorbar=False, vmin=vmin, vmax=vmax, extend='both')
+
+    axes[i//11, i % 11].set_title(str(year[i]))
+    axes[i//11, i % 11].set_xlabel('')
+    axes[i//11, i % 11].set_ylabel('')
+
+axes[3, 9].set_visible(False)
+axes[3, 10].set_visible(False)
 
 # daily pattern for one year
 fig, ax = plt.subplots()
-pet_daily = ds_debruin['pet'].sel(time=slice("1981-01-01", "1982-01-01")).mean(("lon", "lat"))
+pet_daily = ds['pet'].sel(time=slice("1981-01-01", "1981-12-31")).mean(("lon", "lat"))
 pet_daily.plot()
+ax.set_ylabel('pet [mm]')
+ax.set_ylim([0, 4.7])
+
+plt.tight_layout()
 plt.show()
