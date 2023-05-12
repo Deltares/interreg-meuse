97a20755c1c06c40447d9608ce364bd92b391c73
AlessiaRiveros <alessia@riveros.ca>
2023-05-12 11:13:48 +0200

    modify starting model and addition of gauges

--- src/model_building/g_update_rur_model.py
+++ src/model_building/g_update_rur_model.py
@@ -9,13 +9,10 @@ import geopandas as gpd
 from hydromt_wflow import WflowModel
 
 pdir = r"p:\11208719-interreg\wflow"
-# case_base = "d_manualcalib"
-case_base = "f_spwgauges"
+case_base = "d_manualcalib"
 case_update = "g_rur"
-# run_folder = "run_manualcalib"
-run_folder = "run_spwgauges"
-# config_fn = "run_manualcalib.toml"
-config_fn = "run_spwgauges.toml"
+run_folder = "run_manualcalib"
+config_fn = "run_manualcalib.toml"
 run_folder_update = "run_rur"
 
 root = os.path.join(pdir, case_base, run_folder)
@@ -27,6 +24,25 @@ mod.staticgeoms
 
 root_updated = os.path.join(pdir, case_update, run_folder_update)
 mod.set_root(root_updated)
+
+# add gauges
+gauges_path = r"p:\11208719-interreg\data\catchment_rur\LANUV_corrected.csv"
+basename = "hygon"
+
+gauge_toml_header = ["Qriv_hygon", "Q_hygon"]
+gauge_toml_param = ["lateral.river.q_channel_av", "lateral.river.q_av"]
+
+for o in range(len(gauge_toml_param)):
+    gauge_toml_dict = {
+                    "header": gauge_toml_header[o],
+                    "map": f"gauges_{basename}",
+                    "parameter": gauge_toml_param[o],
+                }
+    mod.config["csv"]["column"].append(gauge_toml_dict)
+
+mod.setup_gauges(gauges_fn=gauges_path, index_col="ID", derive_subcatch=True, basename=basename,
+                 gauge_toml_header=gauge_toml_header, gauge_toml_param=gauge_toml_param)
+mod.config["csv"]["column"]
 
 # add reservoirs as lake module but we still need to modify the other parameters!
 mod.setup_lakes(lakes_fn=r'p:\11208719-interreg\data\catchment_rur\gsk3e_reservoirs.gpkg', min_area=0)
@@ -51,8 +67,8 @@ shutil.move(os.path.join(pdir, case_update, run_folder_update, 'staticgeoms'),
             )
 
 # write run bat file
-# path_env_julia = r"c:\Users\bouaziz\.julia\environments\wflow_floodplain1d"
-path_env_julia = r"c:\Users\riveros\.julia\environments\v1.6"
+path_env_julia = r"c:\Users\bouaziz\.julia\environments\wflow_floodplain1d"
+# path_env_julia = r"c:\Users\riveros\.julia\environments\v1.6"
 
 bat_str = f"""
 julia  -t 4 --project={path_env_julia} -e "using Wflow; Wflow.run()" "{run_folder_update}\{run_folder_update}.toml"
