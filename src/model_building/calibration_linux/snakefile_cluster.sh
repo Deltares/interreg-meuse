9fedd0555a5f54032272820ba19e80e030d837b7
Laurene Bouaziz <laurene.bouaziz@deltares.nl>
2023-05-17 10:18:54 +0200

    snake cluster changes

--- src/model_building/calibration_linux/snakefile_cluster.sh
+++ src/model_building/calibration_linux/snakefile_cluster.sh
@@ -13,13 +13,15 @@ source /u/bouaziz/miniconda3/bin/activate hydromt-wflow
 
 #conda config --set channel_priority strict
 
-ROOT="/p/11208186-spw/src/calibration_linux"
+ROOT = "/u/bouaziz/interreg-meuse/src/model_building/calibration_linux"
 cd "${ROOT}"
 
 snakemake -s Snakefile --configfile config/snake_config_model.yml --dag | dot -Tpng > dag_all.png
 
 snakemake --unlock -s Snakefile --configfile config/snake_config_model.yml
-snakemake all -s Snakefile --configfile config/snake_config_model.yml --jobs 20 --latency-wait 60 --wait-for-files --rerun-incomplete --cluster "$SNAKE_SUBMIT_JOB_INTERREG" --directory $PWD
+snakemake all -n -s Snakefile --configfile config/snake_config_model.yml -q --rerun-incomplete
+
+#snakemake all -s Snakefile --configfile config/snake_config_model.yml --jobs 20 --latency-wait 60 --wait-for-files --rerun-incomplete --cluster "$SNAKE_SUBMIT_JOB_INTERREG" --directory $PWD
 
 # --resources mem_mb=70000
 # see dry run (-n) for number of jobs. -q (quiet -- summary) 
